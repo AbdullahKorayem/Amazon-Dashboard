3a3f7201238afb3c5c35a04865445232009e42fd
AbdullahKorayem <abdullah0562007@gmail.com>
2024-03-28 04:57:57 +0200

    Some Enhancements The Overall Code without any Errors

--- src/app/Components/Overview/overview/overview.component.ts
+++ src/app/Components/Overview/overview/overview.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CategoriesService } from 'src/app/Services/Categories-Service/categories.service';
 import { CostumersService } from 'src/app/Services/Customers-Service/costumers.service';
@@ -7,399 +7,399 @@ import { OrdersService } from 'src/app/Services/Orders-Service/orders.service';
 import { ProductServiceService } from 'src/app/Services/Product-Service/product-service.service';
 import { from } from 'rxjs';
 import { Chart } from 'chart.js/auto';
+
 @Component({
-  selector: 'app-overview',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css']
+    selector: 'app-overview',
+    templateUrl: './overview.component.html',
+    styleUrls: ['./overview.component.css'],
 })
 export class OverviewComponent implements OnInit {
-  chart: any = [];
-  doughnutChart: any = []; 
-  public product: any[] = [];
+    chart: any = [];
+    doughnutChart: any = [];
+    public product: any[] = [];
+    public Top5Products: any[] = [];
 
-  public FirebaseOrders: any;
+    public FirebaseOrders: any;
 
-  public Costumers: any[] = [];
+    public Costumers: any[] = [];
 
-  public Totalamount: number = 0;
+    public Totalamount: number = 0;
 
-  public nbOrders: any[] = [];
+    public nbOrders: any[] = [];
 
-  public errorMsg: any[] = [];
+    public errorMsg: any[] = [];
 
-  public cats: any[] = [];
+    public cats: any[] = [];
 
-  // public tags: any[] = [];
+    // public tags: any[] = [];
 
-  // public comments: any[] = [];
+    // public comments: any[] = [];
 
-  constructor(
-    private productService: ProductServiceService,
-    private orderS: OrdersService,
-    private costumerS: CostumersService,
-    private http: HttpClient,
-    private router: Router,
-    private catS: CategoriesService,
+    constructor(
+        private productService: ProductServiceService,
+        private orderS: OrdersService,
+        private costumerS: CostumersService,
+        private http: HttpClient,
+        private router: Router,
+        private catS: CategoriesService
+    ) { }
+    selectedValue: string = 'Pending';
+    LastOrders: string[] = ['ID', 'Name', 'Date', 'Tracking', 'Amount'];
 
-  ) { }
+    customShuffle<T>(array: T[]): T[] {
+        const newArray = [...array];
+        for (let i = newArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+        }
+        return newArray;
+    }
 
-  LastOrders: string[] = [
-    "ID",
-    "Name",
-    "Date",
-    "Tracking",
-    "Amount"
-  ];
+    onSelectChange(event: Event) {
+        const target = event.target as HTMLSelectElement;
+        this.selectedValue = target.value;
+    }
 
+    ngOnInit(): void {
+        this.getData();
 
-
- 
-
-
-
-  ngOnInit(): void {
-
-    
-    this.getData();
-
-    this.chart = new Chart('canvas', {
-      type: 'line',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1,
-          },
-          {
-            label: '# of Votes',
-            data: [1, 2, 3, 9, 7, 3],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    }); 4
-
-    //  Dought Chart
-
-    this.doughnutChart = new Chart('canvas2', {
-      type: 'doughnut',
-      data: {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      },
-      options: {
-        // You can add additional options here if needed
-      }
-    });
+        this.chart = new Chart('canvas', {
+            type: 'line',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Red', 'Blue', 'Yellow' ,'Green', 'Purple', 'Orange', 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [
+                    {
+                        label: '# of Votes 1',
+                        data: [12, 19, 3, 5, 2, 3, 43, 4, 22],
+                        borderWidth: 3, // Thicker line
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        hoverBorderColor: 'rgba(255, 99, 132, 0.8)', // Color on hover
+                        tension: 0.4,
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    },
+                    {
+                        label: '# of Votes 2',
+                        data: [1, 2, 3, 9, 7, 34, 3, 7, 8,32,62,22,1,3],
+                        borderWidth: 3, // Thicker line
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        hoverBorderColor: 'rgba(54, 162, 235, 0.8)', // Color on hover
+                        tension: 0.5,
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+                plugins: {
+                    filler: {
+                        propagate: true
+                    }
+                },
+                elements: {
+                    line: {
+                        fill: 'start',
+                    }
+                }
+            },
+        });
 
 
-  }
 
-  getData(): void {
-    this.getProductData();
-    this.getCostumersData();
-    this.getOrdersData();
-  }
+        //  Dought Chart
 
-  getProductData(): void {
-    from(this.productService.getProducts()).subscribe(
-      (res: any[]) => {
-        this.product = res;
-      },
-      (err: any) => {
-        console.log(err);
-      }
-    );
-  }
+        this.doughnutChart = new Chart('canvas2', {
+            type: 'doughnut',
+            data: {
+                labels: ['Egypt ', 'Blue', 'Yellow'],
+                datasets: [
+                    {
+                        label: 'My First Dataset',
+                        data: [300, 50, 100, 444],
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)',
+                            'rgb(255, 205, 86)',
+                            'rgb(255,75, 86)',
+                        ],
+                        hoverOffset: 4,
+                    },
+                ],
+            },
+            options: {
+                // You can add additional options here if needed
+            },
+        });
+    }
 
-  getCostumersData(): void {
-    from(this.costumerS.getAllUsersFirebase()).subscribe(
-      (res: any[]) => {
-        this.Costumers = res;
-      },
-      (err: any) => {
-        console.log(err);
-      }
-    );
-  }
+    getData(): void {
+        this.getProductData();
+        this.getCostumersData();
+        this.getOrdersData();
+    }
 
-  getOrdersData(): void {
-    from(this.orderS.getAllOrdersFirebase()).subscribe(
-      (res: any[]) => {
-        this.nbOrders = res;
-      },
-      (err: any) => {
-        console.log(err);
-      }
-    );
-  }
+    getProductData(): void {
+        from(this.productService.getProducts()).subscribe(
+            (res: any[]) => {
+                this.product = res;
+
+                const shuffledProducts = this.customShuffle(res);
+                this.Top5Products = shuffledProducts.slice(0, 5);
+                console.log(this.Top5Products);
+            },
+            (err: any) => {
+                console.error(err);
+            }
+        );
+    }
+
+    getCostumersData(): void {
+        from(this.costumerS.getAllUsersFirebase()).subscribe(
+            (res: any[]) => {
+                this.Costumers = res;
+            },
+            (err: any) => {
+                console.log(err);
+            }
+        );
+    }
+
+    getOrdersData(): void {
+        from(this.orderS.getAllOrdersFirebase()).subscribe(
+            (res: any[]) => {
+                this.nbOrders = res;
+            },
+            (err: any) => {
+                console.log(err);
+            }
+        );
+    }
 }
-    
 
-    // this.fetchProducts();
+// this.fetchProducts();
 
-    // this.fetchCostumers();
+// this.fetchCostumers();
 
-    // this.fetchOrders();
+// this.fetchOrders();
 
-    // this.fetchCats();
+// this.fetchCats();
 
-    // this.fetchCats();
+// this.fetchCats();
 
-    // Chart
+// Chart
 
+//fetch acts
+// public fetchCats() {
+//   this.catS.getAllCategories().subscribe(
+//     (data: any[]) => {
+//       this.cats = data;
+//     },
+//     (error) => {
+//       console.error(error);
+//     }
+//   );
+// }
 
-  
+//fetch prods
+// public fetchProducts() {
+//   this.productService.getProducts().subscribe(res => {
 
-  
+//     this.product = res.data;
 
-   
-  
+//   })
+// }
 
+//fetch costumers
+// public fetchCostumers() {
+//   this.costumerS.getCostumer().subscribe((res: any) => {
+//     this.Costumers = res.customers;
+//   }, (err: any) => {
+//     console.log(err);
 
+//   })
+// }
 
-  //fetch acts
-  // public fetchCats() {
-  //   this.catS.getAllCategories().subscribe(
-  //     (data: any[]) => {
-  //       this.cats = data;
-  //     },
-  //     (error) => {
-  //       console.error(error);
-  //     }
-  //   );
-  // }
+//fetch orders
+// public fetchOrders() {
+//   this.orderS.getOrders().subscribe(
+//     (res) => {
+//       this.orders = res;
+//       console.log(res);
 
+//       this.Totalamount = this.calculateTotalAmountWithStatusTrue(this.orders.orders);
+//       this.nbOrders = this.getOrderLength(this.orders.orders);
 
+//     },
+//     (error) => {
+//       console.error('Error fetching orders:', error);
+//     }
+//   );
+// }
 
-  //fetch prods
-  // public fetchProducts() {
-  //   this.productService.getProducts().subscribe(res => {
+//routing
+// navigateToProduct(productId: string) {
+//   this.router.navigate(['/products', productId]);
+// }
 
-  //     this.product = res.data;
+//deleting a product by id
+// public deleteProductById(id: string) {
+//   this.productService.deleteProductById(id).subscribe(
+//     (res) => {
 
-  //   })
-  // }
+//       console.log(res);
+//       this.fetchProducts();
 
-  //fetch costumers
-  // public fetchCostumers() {
-  //   this.costumerS.getCostumer().subscribe((res: any) => {
-  //     this.Costumers = res.customers;
-  //   }, (err: any) => {
-  //     console.log(err);
+//     },
+//     (err) => {
+//       console.log(err);
 
-  //   })
-  // }
+//     }
+//   );
 
-  //fetch orders
-  // public fetchOrders() {
-  //   this.orderS.getOrders().subscribe(
-  //     (res) => {
-  //       this.orders = res;
-  //       console.log(res);
+//   this.product = this.productService.getProducts()
+//   //.subscribe(res => {
 
-  //       this.Totalamount = this.calculateTotalAmountWithStatusTrue(this.orders.orders);
-  //       this.nbOrders = this.getOrderLength(this.orders.orders);
+//   //   this.product = res.data;
 
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching orders:', error);
-  //     }
-  //   );
-  // }
+//   // })
+// }
 
-  //routing
-  // navigateToProduct(productId: string) {
-  //   this.router.navigate(['/products', productId]);
-  // }
+//date formateur
+// public formatReadableDate(dateString: any) {
+//   const options: any = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
 
-  //deleting a product by id
-  // public deleteProductById(id: string) {
-  //   this.productService.deleteProductById(id).subscribe(
-  //     (res) => {
+//   const date = new Date(dateString);
 
-  //       console.log(res);
-  //       this.fetchProducts();
+//   return date.toLocaleString('en-US', options);
+// }
 
-  //     },
-  //     (err) => {
-  //       console.log(err);
+//get order length
+// public getOrderLength(orders: any[]): number {
+//   return orders.length;
+// }
 
+//calculate the amount of an activates orders
+// public calculateTotalAmountWithStatusTrue(orders: any[]): number {
+//   let totalAmount = 0;
+//   for (const order of orders) {
 
-  //     }
-  //   );
+//     if (order.status === true) {
+//       totalAmount += order.totalAmount;
+//     }
 
-  //   this.product = this.productService.getProducts()
-  //   //.subscribe(res => {
+//   }
+//   return totalAmount;
+// }
 
-  //   //   this.product = res.data;
+//toogle the activate button and the status attribut in order scheme
+// public toggleOrderStatusById(id: string) {
 
-  //   // })
-  // }
+//   const orderToUpdate = this.orders.orders.find((order: any) => order._id === id);
 
-  //date formateur
-  // public formatReadableDate(dateString: any) {
-  //   const options: any = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+//   if (orderToUpdate) {
 
-  //   const date = new Date(dateString);
+//     const currentStatus = orderToUpdate.status;
+//     const newStatus = !currentStatus;
 
-  //   return date.toLocaleString('en-US', options);
-  // }
+//     this.orderS?.updateOrderStatusById(id, newStatus).subscribe(
+//       (res) => {
 
+//         console.log(res);
+//         orderToUpdate.status = newStatus;
+//         this.fetchOrders();
 
-  //get order length
-  // public getOrderLength(orders: any[]): number {
-  //   return orders.length;
-  // }
+//       },
+//       (err) => {
 
-  //calculate the amount of an activates orders
-  // public calculateTotalAmountWithStatusTrue(orders: any[]): number {
-  //   let totalAmount = 0;
-  //   for (const order of orders) {
+//         console.log(err);
 
-  //     if (order.status === true) {
-  //       totalAmount += order.totalAmount;
-  //     }
+//       }
+//     );
+//   }
+// }
 
-  //   }
-  //   return totalAmount;
-  // }
+// public deleteOrderById(id: string) {
 
-  //toogle the activate button and the status attribut in order scheme
-  // public toggleOrderStatusById(id: string) {
+//   this.orderS.deleteOrderById(id).subscribe(
+//     (res) => {
 
-  //   const orderToUpdate = this.orders.orders.find((order: any) => order._id === id);
+//       console.log(res);
 
-  //   if (orderToUpdate) {
+//       this.orders.orders = this.orders.orders.filter((order: any) => order._id !== id);
+//       this.updateProductQuantities(res, false);
+//       this.fetchOrders();
 
-  //     const currentStatus = orderToUpdate.status;
-  //     const newStatus = !currentStatus;
+//     },
+//     (err) => {
 
-  //     this.orderS?.updateOrderStatusById(id, newStatus).subscribe(
-  //       (res) => {
+//       console.log(err);
 
-  //         console.log(res);
-  //         orderToUpdate.status = newStatus;
-  //         this.fetchOrders();
+//     }
+//   );
+// }
 
-  //       },
-  //       (err) => {
+//it works fine
+// private updateProductQuantities(result: any, status?: boolean) {
+//   for (const updatedProduct of result.order.products) {
 
-  //         console.log(err);
+//     const productId = updatedProduct.product._id;
+//     const allQuantity = parseInt(updatedProduct.product.quantity, 10);
+//     const subQuantity = parseInt(updatedProduct.quantity, 10);
 
-  //       }
-  //     );
-  //   }
-  // }
+//     const newQuantity = status ? allQuantity - subQuantity : allQuantity + subQuantity;
+//     const updateUrl = `http://localhost:3000/api/v1/products/product/${productId}`;
 
-  // public deleteOrderById(id: string) {
+//     console.log(updatedProduct, newQuantity);
 
-  //   this.orderS.deleteOrderById(id).subscribe(
-  //     (res) => {
+//     this.http.put(updateUrl, { quantity: newQuantity })
+//       .subscribe(
+//         (response) => {
 
-  //       console.log(res);
+//           console.log(response);
 
-  //       this.orders.orders = this.orders.orders.filter((order: any) => order._id !== id);
-  //       this.updateProductQuantities(res, false);
-  //       this.fetchOrders();
+//         },
+//         (error) => {
 
-  //     },
-  //     (err) => {
+//           if (error.status === 404) {
+//             console.log('Product not found.');
+//           } else {
+//             console.error(error);
+//           }
 
-  //       console.log(err);
+//         }
+//       );
+//   }
+// }
 
-  //     }
-  //   );
-  // }
+//delete cosutmer by id and also delete the related orders
+// public deleteCostumer(id: string) {
+//   this.costumerS.deleteCostumerById(id).subscribe(
+//     (res: any) => {
 
-  //it works fine
-  // private updateProductQuantities(result: any, status?: boolean) {
-  //   for (const updatedProduct of result.order.products) {
+//       this.fetchCostumers();
 
-  //     const productId = updatedProduct.product._id;
-  //     const allQuantity = parseInt(updatedProduct.product.quantity, 10);
-  //     const subQuantity = parseInt(updatedProduct.quantity, 10);
+//     }, (err: any) => {
 
+//       console.log(err);
 
+//     }
+//   )
+// }
 
-  //     const newQuantity = status ? allQuantity - subQuantity : allQuantity + subQuantity;
-  //     const updateUrl = `http://localhost:3000/api/v1/products/product/${productId}`;
+// //delete a category by id
+// public deleteCat(id: string) {
+//   this.catS.deleteCategoryById(id).subscribe(
+//     (data) => {
 
-  //     console.log(updatedProduct, newQuantity);
+//       console.log(data);
+//       // this.fetchCats();
 
-
-  //     this.http.put(updateUrl, { quantity: newQuantity })
-  //       .subscribe(
-  //         (response) => {
-
-  //           console.log(response);
-
-  //         },
-  //         (error) => {
-
-  //           if (error.status === 404) {
-  //             console.log('Product not found.');
-  //           } else {
-  //             console.error(error);
-  //           }
-
-  //         }
-  //       );
-  //   }
-  // }
-
-  //delete cosutmer by id and also delete the related orders 
-  // public deleteCostumer(id: string) {
-  //   this.costumerS.deleteCostumerById(id).subscribe(
-  //     (res: any) => {
-
-  //       this.fetchCostumers();
-
-  //     }, (err: any) => {
-
-  //       console.log(err);
-
-
-  //     }
-  //   )
-  // }
-
-
-
-  // //delete a category by id
-  // public deleteCat(id: string) {
-  //   this.catS.deleteCategoryById(id).subscribe(
-  //     (data) => {
-
-  //       console.log(data);
-  //       // this.fetchCats();
-
-  //     },
-  //     (error) => {
-  //       console.error(error);
-  //     }
-  //   );
-  // }
-
+//     },
+//     (error) => {
+//       console.error(error);
+//     }
+//   );
+// }
---
+++ src/app/Directive/select-styling.directive.ts
@@ -0,0 +1,38 @@
+// import { Directive, ElementRef, HostListener } from '@angular/core';
+
+// @Directive({
+//   selector: 'select[appSelectStyling]'
+// })
+// export class SelectStylingDirective {
+
+//   constructor(private el: ElementRef) { }
+
+//   @HostListener('change') onChange() {
+//   //   const selectedOption = this.el.nativeElement.options[this.el.nativeElement.selectedIndex];
+//   //   switch (selectedOption.value) {
+//   //     case 'Pending':
+//   //       this.applyStyles('text-blue-600', 'bg-blue-200');
+//   //       break;
+//   //     case 'Delivered':
+//   //       this.applyStyles('text-green-600', 'bg-green-200');
+//   //       break;
+//   //     case 'Shipping':
+//   //       this.applyStyles('text-blue-600', 'bg-blue-200');
+//   //       break;
+//   //     default:
+//   //       // Do nothing to preserve existing styling
+//   //       break;
+//   //   }
+//   // }
+
+//   // private applyStyles(...styles: string[]) {
+//   //   // Remove any existing styling
+//   //   this.clearStyles();
+//   //   // Apply new styles
+//   //   this.el.nativeElement.classList.add(...styles);
+//   // }
+
+//   // private clearStyles() {
+//   //   this.el.nativeElement.classList.remove('text-blue-600', 'bg-blue-200', 'text-green-600', 'bg-green-200');
+//   // }
+// }
