af06b025b70816f9ac8cb687e2d933bdb188d920
AbdullahKorayem <abdullah0562007@gmail.com>
2024-04-21 23:27:48 +0200

    fixing UserRegister

--- src/app/Components/user-register/user-register.component.ts
+++ src/app/Components/user-register/user-register.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-Register-login',
   templateUrl: './user-register.component.html',
-  styleUrls: ['./user-Register.component.css']
+  
 })
 export class UserRegisterComponent {
   public User: FormGroup;
