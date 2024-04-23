4efa66b16fcf1ee11e9fdceea61dbf944b0d6028
AbdullahKorayem <abdullah0562007@gmail.com>
2024-04-23 12:11:19 +0200

    admins +2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: {
+      'xxxs': '330px',
+      'xxs': '380px',
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       rotate: {
         '20': '20deg',
